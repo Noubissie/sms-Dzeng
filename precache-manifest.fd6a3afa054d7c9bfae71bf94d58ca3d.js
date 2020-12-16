bb79c76c32a97f978e8a77d095d732a61daa9cf4
NOUBISSIE <landryplacid@gmail.com>
2020-12-16 12:52:57 +0000

    Updates

---
+++ precache-manifest.fd6a3afa054d7c9bfae71bf94d58ca3d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0ff1827b41f83a90e1b3f91691eb6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "867603b954a8985dcf51",
+    "url": "/static/css/2.fb141226.chunk.css"
+  },
+  {
+    "revision": "58417843c030b6cda7bb",
+    "url": "/static/css/main.101ecae0.chunk.css"
+  },
+  {
+    "revision": "867603b954a8985dcf51",
+    "url": "/static/js/2.cac77d41.chunk.js"
+  },
+  {
+    "revision": "fbf5f832e7a7275831e325b7e9182efe",
+    "url": "/static/js/2.cac77d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58417843c030b6cda7bb",
+    "url": "/static/js/main.08435cf3.chunk.js"
+  },
+  {
+    "revision": "9b01bea236db5dd6fe52",
+    "url": "/static/js/runtime-main.59e73e23.js"
+  },
+  {
+    "revision": "13b15e96ff507d91347b0f85b275c0be",
+    "url": "/static/media/Teacher.13b15e96.svg"
+  },
+  {
+    "revision": "21f7233fcfb5b0e7b0fcd4b672a1d60d",
+    "url": "/static/media/defaultImage.21f7233f.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/favicon.d41d8cd9.ico"
+  },
+  {
+    "revision": "5d6967cb5d6a33b8300a7500f3631bca",
+    "url": "/static/media/github.5d6967cb.svg"
+  },
+  {
+    "revision": "98ac656e7064c2add3efe171c528c3cb",
+    "url": "/static/media/moon.98ac656e.svg"
+  },
+  {
+    "revision": "a5b800e3f4f219988a517d61959282f3",
+    "url": "/static/media/parents.a5b800e3.svg"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/static/media/roboto-v18-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "376e0950b361fbd3b09508031f498de5",
+    "url": "/static/media/roboto-v18-latin-700.376e0950.eot"
+  },
+  {
+    "revision": "57888be7f3e68a7050452ea3157cf4de",
+    "url": "/static/media/roboto-v18-latin-700.57888be7.svg"
+  },
+  {
+    "revision": "cae5027f600d2a0d88ac309655618e31",
+    "url": "/static/media/roboto-v18-latin-700.cae5027f.ttf"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/static/media/roboto-v18-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "372d0cc3288fe8e97df49742baefce90",
+    "url": "/static/media/roboto-v18-latin-regular.372d0cc3.ttf"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/static/media/roboto-v18-latin-regular.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "68889c246da2739681c1065d15a1ab0b",
+    "url": "/static/media/roboto-v18-latin-regular.68889c24.eot"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/static/media/roboto-v18-latin-regular.8681f434.svg"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/static/media/roboto-v18-latin-regular.bafb105b.woff"
+  },
+  {
+    "revision": "882e4d2178216cc2e8cd23827696e888",
+    "url": "/static/media/stackoverflow.882e4d21.svg"
+  },
+  {
+    "revision": "83503bdd6b8a2c674341f8cc65bdd452",
+    "url": "/static/media/students.83503bdd.svg"
+  },
+  {
+    "revision": "2a5a3b478475adb24f3f8c8093a6267a",
+    "url": "/static/media/sun.2a5a3b47.svg"
+  },
+  {
+    "revision": "d65716341ae94131ea2bbc281ced6422",
+    "url": "/static/media/telegram.d6571634.svg"
+  },
+  {
+    "revision": "c87b2aef17c88cc57501c9a12ac4a666",
+    "url": "/static/media/twitter.c87b2aef.svg"
+  }
+]);
